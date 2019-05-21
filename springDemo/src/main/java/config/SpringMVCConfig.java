3f2497f66f037fe7a2e22671bd82a3dd46ed5e71
P0TAT0Ss <hanlinjue@gmail.com>
2019-05-21 14:52:07 +0800

    bulabula

--- springDemo/src/main/java/config/SpringMVCConfig.java
+++ springDemo/src/main/java/config/SpringMVCConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.mvc.Controller;
 
 @Configuration
@@ -12,5 +14,11 @@ import org.springframework.web.servlet.mvc.Controller;
         useDefaultFilters = false,
         includeFilters =
                 {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = {Controller.class})})
-public class SpringMVCConfig {
+public class SpringMVCConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+    }
 }
--- springDemo/src/main/java/per/han/controller/userController.java
+++ springDemo/src/main/java/per/han/controller/userController.java
@@ -2,8 +2,19 @@ package per.han.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/user")
-public class userController {
+public class userController extends AbstractController {
+    @Override
+    protected ModelAndView handleRequestInternal
+            (HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        return null;
+    }
 }
