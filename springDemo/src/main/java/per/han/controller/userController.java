605ba2246b2e43ad99c53ff1c82131c50835c767
potat-OS <hanlinjue@gmail.com>
2019-06-11 18:22:58 +0800

    some changes

--- springDemo/src/main/java/per/han/controller/userController.java
+++ springDemo/src/main/java/per/han/controller/userController.java
@@ -1,20 +1,7 @@
 package per.han.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @Controller
-@RequestMapping("LogIn")
-public class userController extends AbstractController {
-    @Override
-    protected ModelAndView handleRequestInternal
-            (HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-        return null;
-    }
+public class userController {
 }
